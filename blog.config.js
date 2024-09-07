95296773900f87951842db4eed224328964744e2
Kadriye <Kadriye@yeah.net>
2024-09-07 15:50:22 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -14,8 +14,8 @@ const BLOG = {
   since: 2021, // If leave this empty, current year will be used.
   postsPerPage: 7,
   sortByDate: false,
-  showAbout: true,
-  showArchive: true,
+  showAbout: false,
+  showArchive: false,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://x.com/83838052_',
