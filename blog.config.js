c9c8585ad5cba1a1a8b8de144d1955527e77ccaf
Kadriye <Kadriye@yeah.net>
2024-09-07 15:03:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
   title: 'Kadriye Bolg',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  author: 'Kadriye',
+  email: 'luwei.br@gmail.com',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
